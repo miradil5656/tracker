b67e7cf9b635aab14a6cc79638f9e145333a9a88
miradil5656 <miradild404@gmail.com>
2023-10-24 22:30:49 +0900

    style the active link

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,18 +1,31 @@
+'use client'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 import {AiFillBug} from 'react-icons/ai'
-//AiFillBug
+import classNames from 'classnames'
 const Navbar = () => {
+
+    const currentPath = usePathname()
+   
 
     const links=[
         {lable:'Dashboard',href:'/'},
         {lable:'Issues',href:'/issues'}
     ]
+
+    
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-20 items-center text-2xl'>
         <Link href="/" ><AiFillBug /></Link>
-        <ul className='flex space-x-6'>
-            {links.map((link,index)=>(<Link href={link.href} key={index} className='text-zinc-500 hover:text-zinc-800 transition-colors'>{link.lable}</Link>))}
+        <ul className='flex space-x-10'>
+            {links.map((link,index)=>(<Link href={link.href} key={index} className={
+                classNames({
+                    'text-zinc-900 font-bold': link.href === currentPath,
+                    'text-zinc-500' : link.href !== currentPath,
+                    'hover:text-zinc-800 transition-colors' : true
+                })
+            }>{link.lable}</Link>))}
             
           
             
